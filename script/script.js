2ecbc6e11a5b6d19895197e39a6c4552325a6a69
samuelauto <samuelme@automatica.edu.cujae.cu>
2024-06-06 04:39:14 -0400

    Arreglar detalles en el html

--- script/script.js
+++ script/script.js
@@ -8,7 +8,7 @@ function Gameboard(){
     const getBoard = () => board; //Funcion para obtener el tablero
 
     //Funcion para actualizar el tablero con la casilla marcada
-    const refreshBoard = (nrow,ncolumn,valueCell) => {
+    const refreshBoard = (nrow,ncolumn,valueCell,id) => {
         //Asegurar de que la casilla seleccionada no este llena
         for(let i=0;i<row;i++){
             for(let j=0;j<column;j++){
@@ -19,6 +19,10 @@ function Gameboard(){
             }
         }
         board[nrow][ncolumn] = valueCell;
+        //Colocar el texto valueCell dentro de la casilla que se escribio
+        const button = document.getElementById(id);
+        button.textContent = valueCell;
+
         return 1;
     }
 
@@ -41,9 +45,9 @@ function Players(name,choise){
 }
 
 
-function GameControlling(){
+function GameControlling(name1,name2){
     const board = Gameboard()
-    const players = [Players("Samuel","X"),Players("La pura","0")];
+    const players = [Players(name1,"X"),Players(name2,"0")];
     let countRounds = 0;
     //Usar cero para designar jugador con cerito y 1 para seleccionar jugador con X
     //Empezar con el jugador de las cruces
@@ -115,10 +119,7 @@ function GameControlling(){
         }
     }
 
-    const roundPlay = () =>{
-        //console.log(activePlayer);
-        let row = parseInt(prompt("Introduce fila: "),10);
-        let column = parseInt(prompt("Introduce columna: "),10);
+    const roundPlay = (row,column,id) =>{
         //switchPlayer();//Cambio de jugador al empezar la ronda
         let valueCell = ''
         //Si acivePlayer se encuentra en 0 juegan los ceritos, si esta en 1 juegan las cruces
@@ -135,41 +136,40 @@ function GameControlling(){
         countRounds++;
         let playAgain = 0
         do{ //se devuelve cero cdo el jugador juega mal
-            playAgain = board.refreshBoard(row,column,valueCell); //Se sale de este ciclo cdo el jugador juege bien
+            playAgain = board.refreshBoard(row,column,valueCell,id); //Se sale de este ciclo cdo el jugador juege bien
             //esto es temporal,solo para demostrar que funciona bien
             if(playAgain===0){
-                row = prompt("Introduce fila nuevamente sanaco:")
-                column = prompt("Introduce columna nuevamente sanaco: ")
+                return 0;
             }
         }while( playAgain===0)
         if (countRounds >= 3){
             const winner = finishGame();
-            if(winner===1){
-                console.log("Empate");
-                console.log(board.getBoard());
+            if(winner===1){ //empate
+                return "Empate"
             }
             else if(winner!==0){
                 if(winner === "X"){
-                    console.log(players[0].name);
-                    console.log(board.getBoard());
+                    return players[0].name
                 }
                 else{
-                    console.log(players[1].name);
-                    console.log(board.getBoard());
+                    return players[1].name
                 }
             }
-            else{
-                switchPlayer();
-                roundPlay();
-            }
+            // else{
+            //     switchPlayer();
+            //     roundPlay();
+            // }
         }
-        else{
-            switchPlayer();
-            roundPlay();
-        }        
+        switchPlayer();
+        // else{
+        //     switchPlayer();
+        //     roundPlay();
+        // }        
     }
+
     
-    roundPlay();
+
+    return {roundPlay}
 }
 
 //GameControlling();
@@ -178,5 +178,95 @@ function ScreenControlling(){
     //Funcion para manipular el DOM, recibir entradas de cada jugador(nombre y seleccion)
     //Obtener coordenada de cada boton, y pasarlo por el GameControlling
     //Elaborar mensaje de jugador ganador o empate una vez terminado el GameControlling,respetando el funcionamiento de este game controlling
+    const namePlayer1 = document.querySelector('#name1').value;
+    const namePlayer2 = document.querySelector('#name2').value;
     
+    const boardPlayers = document.querySelector('.players');//Aqui es a donde va a ir el append child de que se jugo mal,esto debe durar un pequeño tiempo
+    const containerBoard = document.querySelector('.container-board');
+    const winDiv = document.createElement('div');
+    winDiv.className = 'winDiv';
+
+   const game = GameControlling(namePlayer1,namePlayer2);
+
+   const updateScreen = () => {
+        const options = document.querySelectorAll('.board-item');
+
+        options.forEach((button) => {
+            button.addEventListener('click',function(){
+                switch (button.id) {
+                    case '1': console.log("lee aqui")
+                             documentRefresh(0,0,'1');
+                             break;
+                    case '2': documentRefresh(0,1,'2');
+                            break;
+                    case '3': documentRefresh(0,2,'3');
+                            break;
+                    case '4': documentRefresh(1,0,'4');
+                            break;
+                    case '5': documentRefresh(1,1,'5');
+                            break;
+                    case '6': documentRefresh(1,2,'6');
+                            break;
+                    case '7': documentRefresh(2,0,'7');
+                            break;
+                    case '8': documentRefresh(2,1,'8');
+                            break;
+                    case '9': documentRefresh(2,2,'9');
+
+                    default: break;
+
+                }
+            })
+        }) 
+   }
+
+    const documentRefresh = (row,column,id) => {
+        if(game.roundPlay(row,column,id)===0){//Casilla LLena
+            const warningBox = document.createElement("div");
+            warningBox.className = "warning";
+            warningBox.textContent = "Esa casilla ya se encuentra ocupada,juegue otra";
+
+            if (document.body.contains(warningBox)) {
+                clearTimeout(warningTimeout);
+                } else {
+                boardPlayers.appendChild(warningBox);
+                }
+        
+            warningTimeout = setTimeout(() => {
+            warningBox.parentNode.removeChild(warningBox);
+            warningTimeout = -1;
+            },2000);
+        }
+        else if(game.roundPlay(row,column,id)==="X"){
+            winDiv.textContent = "El ganador es: " + namePlayer1;
+            winDiv.appendChild(containerBoard);
+        }
+        else if(game.roundPlay(row,column,id)==="0"){
+            winDiv.textContent = "El ganador es: " + namePlayer2;
+            winDiv.appendChild(containerBoard);
+        }
+        else if(game.roundPlay(row,column,id) === 1){
+            winDiv.textContent = "Empate";
+            winDiv.appendChild(containerBoard);
+        }
+    }
+
+   updateScreen();
+
 }
+
+const initPlay = document.querySelector('.submit').addEventListener('click',function(){
+    ScreenControlling();
+})
+
+const resetPlay = document.querySelector('.reset').addEventListener('click',function(){
+    boardItem = document.querySelectorAll('.board-item')
+    name1 = document.getElementById('name1')
+    name2 = document.getElementById('name2')
+
+    boardItem.forEach((item)=>{
+        item.textContent = ""
+    })
+    name1.textContent = "";
+    name2.textContent = "";
+})
